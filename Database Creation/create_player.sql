b2985da4c32a03ebbcad1d2bae8e8d7c33a35827
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2023-04-17 13:56:11 -0400

    added primary key

--- Database Creation/create_player.sql
+++ Database Creation/create_player.sql
@@ -1,5 +1,5 @@
 CREATE TABLE WR_Prospects.Player (
-    Name VARCHAR(255),
+    Name VARCHAR(255) PRIMARY KEY,
     College_Conference VARCHAR(255),
     College_Team VARCHAR(255),
     Overall_Pick INT,
