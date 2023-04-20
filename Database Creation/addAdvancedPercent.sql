8be376f73592196a02652ecd5559c724a4e23157
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2023-04-20 15:46:55 -0400

    added other advanced percent

---
+++ Database Creation/addAdvancedPercent.sql
@@ -0,0 +1,10 @@
+ALTER TABLE WR_Prospects.Advanced_Stats
+ADD DOM_Percentile FLOAT;
+
+ALTER TABLE WR_Prospects.Advanced_Stats
+ADD LOC_Percentile FLOAT;
+
+ALTER TABLE WR_Prospects.Advanced_Stats
+ADD RAS_Percentile FLOAT;
+
+
