61cfdfbb3968a07909905d73701d286b64946f95
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2023-04-20 16:25:24 -0400

    updated stats schema

--- Database Creation/create_stats.sql
+++ Database Creation/create_stats.sql
@@ -1,6 +1,7 @@
 CREATE TABLE WR_Prospects.Stats (
     Name VARCHAR(255) REFERENCES Player(Name),
     Receiving_Yards INT,
+    Yards_Percentile FLOAT,
     Receptions INT,
     Yards_Per_Reception FLOAT,
     Receiving_Touchdowns INT,
