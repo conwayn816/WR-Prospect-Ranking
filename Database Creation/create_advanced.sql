21b5ba03275d6414faf415d5e7cb190e0b32b41f
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2023-04-20 16:20:39 -0400

    new schema

--- Database Creation/create_advanced.sql
+++ Database Creation/create_advanced.sql
@@ -1,8 +1,12 @@
 CREATE TABLE WR_Prospects.Advanced_Stats (
     Name VARCHAR(255) REFERENCES Player(Name),
     College_Dominator_Rating FLOAT,
+    DOM_Percentile FLOAT,
     Breakout_Age FLOAT,
+    BA_Percentile FLOAT
     College_Level_of_Competition FLOAT,
+    LOC_Percentile FLOAT,
     RAS_Score FLOAT,
+    RAS_Percentile FLOAT,
     PRIMARY KEY (Name)
 );
