260558c75769f9a791f627c550169424f05f2d16
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2023-04-20 14:20:41 -0400

    ras percentile

--- percentiles.sql
+++ percentiles.sql
@@ -2,14 +2,14 @@ SELECT s.Name, s.Receiving_Yards,
 ROUND(
     PERCENT_RANK() OVER (ORDER BY Receiving_Yards),2
 ) yards_percentile,
-a.College_Dominator_Rating, ROUND(
-    PERCENT_RANK() OVER (ORDER BY College_Dominator_Rating),2
-) DOM_percentile,
+a.RAS_Score, ROUND(
+    PERCENT_RANK() OVER (ORDER BY RAS_Score),2
+) RAS_percentile,
 a.Breakout_Age, ROUND(
     PERCENT_RANK() OVER (ORDER BY Breakout_Age),2
 ) BA_percentile
 FROM WR_Prospects.Stats s
     INNER JOIN WR_Prospects.Advanced_Stats a
     ON s.Name = a.Name
-WHERE a.Breakout_Age IS NOT NULL
+WHERE a.RAS_Score > 0
     ORDER BY s.Name;
