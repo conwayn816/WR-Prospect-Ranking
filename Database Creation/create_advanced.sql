cadbd67628a87c8472fbe3d95c747b9b7bfdb3a6
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2023-04-20 16:30:08 -0400

    needed comma

--- Database Creation/create_advanced.sql
+++ Database Creation/create_advanced.sql
@@ -3,7 +3,7 @@ CREATE TABLE WR_Prospects.Advanced_Stats (
     College_Dominator_Rating FLOAT,
     DOM_Percentile FLOAT,
     Breakout_Age FLOAT,
-    BA_Percentile FLOAT
+    BA_Percentile FLOAT,
     College_Level_of_Competition FLOAT,
     LOC_Percentile FLOAT,
     RAS_Score FLOAT,
