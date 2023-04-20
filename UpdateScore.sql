3180a8f2d89c6761a5a508f0889d96686548be6a
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2023-04-20 17:02:15 -0400

    advanced function exists now

---
+++ UpdateScore.sql
@@ -0,0 +1,15 @@
+
+-- update the Score column for each player
+UPDATE WR_Prospects.Player p
+JOIN (
+    SELECT p.Name, 
+           SUM(s.Yards_Percentile + a.DOM_Percentile + (a.LOC_Percentile * 3) 
+               + (c.Conference_Strength * 1.5) + (s.Receiving_Touchdowns * 0.0132) + a.BA_Percentile 
+               + (CASE WHEN a.RAS_Score IS NULL THEN 0 ELSE a.RAS_Percentile * 2 END)) AS Score
+    FROM WR_Prospects.Player p
+    JOIN WR_Prospects.Stats s ON p.Name = s.Name
+    JOIN WR_Prospects.Advanced_Stats a ON p.Name = a.Name
+    JOIN WR_Prospects.Conferences c ON p.Conference = c.Conference_Name
+    GROUP BY p.Name
+) AS t ON p.Name = t.Name
+SET p.Score = t.Score;
